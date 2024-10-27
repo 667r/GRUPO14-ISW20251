71899beff39ce456eea8381272014cd38ee22591
Mariano <mariano.varas@usm.cl>
2024-10-27 23:18:24 +0000

    Hito 3

--- vigifia/vigifia_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VigifiaAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'vigifia_app'
--- vigifia/vigifia_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('boletines/', views.boletines, name='boletines'),
-    path('crear-boletin/', views.crear_boletin, name='crear_boletin'),
-]
-
